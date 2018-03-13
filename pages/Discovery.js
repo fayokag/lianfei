655f49e52658968b29d39e1118e7f50bd24ad7de
fayokag <287919935@qq.com>
2018-03-13 15:57:50 +0800

    首页增加tab并绘画

--- pages/Discovery.js
+++ pages/Discovery.js
@@ -9,8 +9,10 @@ import {
     TouchableOpacity,
     Dimensions,
 } from 'react-native';
+import ScrollableTabView, { ScrollableTabBar, DefaultTabBar } from 'react-native-scrollable-tab-view';
 import RollingPic from "../components/RollingPic";
-import {blue} from "../utils/colors";
+import All from "./tab/All";
+import { blue } from "../utils/colors";
 
 const images = [
     require('../imgs/shouye/banner/2.jpg'),
@@ -18,20 +20,20 @@ const images = [
     require('../imgs/shouye/banner/1.jpg'),
 ];
 
-const headerFind =(
+const headerFind = (
     <TouchableOpacity>
         <Image
-            style={{marginLeft: 92, height: 40, width: 200, resizeMode: 'contain',}}
+            style={{ marginLeft: 92, height: 40, width: 200, resizeMode: 'contain', }}
             source={require('../imgs/discovery/btn_not_find.png')}
             onPress={() => console.log(Dimensions.get('window').width)}
         />
     </TouchableOpacity>
 )
 
-const headerNotFind =(
+const headerNotFind = (
     <TouchableOpacity>
         <Image
-            style={{marginLeft: 92, height: 40, width: 200, resizeMode: 'contain',}}
+            style={{ marginLeft: 92, height: 40, width: 200, resizeMode: 'contain', }}
             source={require('../imgs/discovery/btn_not_find.png')}
             onPress={() => console.log(Dimensions.get('window').width)}
         />
@@ -50,7 +52,7 @@ export default class Discovery extends React.Component {
     static navigationOptions = {
         // headerLeft:this.state.discovery === true ? headerFind : headerNotFind,
         headerStyle: {
-            backgroundColor: blue,
+            backgroundColor: 'blue',
         },
     }
 
@@ -64,15 +66,34 @@ export default class Discovery extends React.Component {
 
         if (this.state.discovery) {
             return (
-                <ScrollView style={{flex: 1}}>
-                    <RollingPic images={images}/>
+                <ScrollView style={{ flex: 1,backgroundColor:'#fff' }}>
+                    <RollingPic images={images} />
+                    <ScrollableTabView
+                        style={{ backgroundColor: '#fff' }}
+                        tabBarUnderlineStyle={{ backgroundColor: '#2E89D9' }}
+                        tabBarActiveTextColor='#2E89D9'
+                        tabBarInactiveTextColor='#999'
+                        locked={true}
+                        renderTabBar={() => <DefaultTabBar tabStyle={{ paddingBottom: 0 }} />}
+                    >
+                        <All tabLabel='全部'
+                            navigation={this.props.navigation} />
+                        <All tabLabel='菜鸟聊吧'
+                            navigation={this.props.navigation} />
+                        <All tabLabel='问答区'
+                            navigation={this.props.navigation} />
+                        <All tabLabel='连飞活动'
+                            navigation={this.props.navigation} />
+                        <All tabLabel='招募'
+                            navigation={this.props.navigation} />
+                    </ScrollableTabView>
                 </ScrollView>
             )
         }
         else {
             return (
-                <ScrollView style={{flex: 1}}>
-                    <RollingPic images={images}/>
+                <ScrollView style={{ flex: 1 }}>
+                    <RollingPic images={images} />
                 </ScrollView>
             )
         }
